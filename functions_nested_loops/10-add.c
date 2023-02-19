8d49f134dd979b9da5ce996639232038bef64a11
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-02-19 09:58:30 -0800

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - ads two integers
+ *
+ * Return: the result of the addition
+ *
+ * @a: variable
+ *
+ * @b: variable
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+_putchar(c + '0');
+return (c);
+}
