3957ea1ff76915232be3b4d92ec0ae7ee333f38d
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-02-28 11:13:39 -0800

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: variable
+ */
+void puts_half(char *str)
+{
+	int a = 0, b;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = (a / 2);
+	}
+	else
+	{
+		b = ((a - 1) / 2);
+	}
+	for (; b < a; b++)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
