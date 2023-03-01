ef64228b216b72d3ee510a951dedef557cc05e7a
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-01 08:24:42 -0800

    2.2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strncpy- copies a string
+ *
+ * Return: pointer to dest
+ *
+ * @dest: variable
+ *
+ * @src: variable
+ *
+ * @n: variable
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while (src[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+
+	while (b <= n)
+	{
+		dest[b] = src[b];
+		b++;
+	}
+	if (b == n + 1)
+	{
+		dest[b] = '\0';
+	}
+	return (dest);
+}
