abd3d6896306436021425ce969ea69ac2c2878c7
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-01 10:48:12 -0800

    3.2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,37 +10,24 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0, c, d = 0, r;
+	int a, r;
 
-	while (s1[a] != '\0')
+	a = 0;
+	while (s1[a] != '\0' || s2[a] != '\0')
 	{
-		a++;
-	}
-	while (s2[b] != '\0')
-	{
-		b++;
-	}
-	if (a == b)
-	{
-		for (c = 0; s2[c] != '\0'; c++)
+		if (s1[a] == s2[a])
 		{
-			if (s1[c] == s2[c])
-			{
-				d++;
-			}
-			if (d == c)
-			{
-				r = 0;
-			}
-			else if (d < c)
-			{
-				r = -1;
-			}
-			else
-			{
-				r = 1;
-			}
+			r = 0;
 		}
+		else if (s1[a] < s2[a])
+		{
+			r = -13;
+		}
+		else
+		{
+			r = 13;
+		}
+		a++;
 	}
 	return (r);
 }
