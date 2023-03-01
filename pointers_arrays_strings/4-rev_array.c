3d33f5508bc9a69223ca84748a846480111df61f
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-01 11:28:46 -0800

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array of integers
+ *
+ * @a: array
+ *
+ * @n: variable
+ */
+void reverse_array(int *a, int n)
+{
+	int o, p;
+
+	for (o = 0; o < n / 2; o++)
+	{
+		p = a[o];
+		a[o] = a[n - o - 1];
+		a[n - o - 1] = p;
+	}
+}
