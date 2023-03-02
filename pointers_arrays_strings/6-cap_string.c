4937c6c5708a533eb92590e78325fe30b4d5ad95
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-01 18:06:55 -0800

    6.2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ *
+ * Return: char
+ *
+ * @s: string
+ */
+char *cap_string(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a])
+	{
+	while (!(s[a] >= 'a' && s[a] <= 'z'))
+	{
+		a++;
+	}
+	if (s[a - 1] == ' ' ||
+			s[a - 1] == '\t' ||
+			s[a - 1] == '\n' ||
+			s[a - 1] == ',' ||
+			s[a - 1] == ';' ||
+			s[a - 1] == '.' ||
+			s[a - 1] == '!' ||
+			s[a - 1] == '?' ||
+			s[a - 1] == '"' ||
+			s[a - 1] == '(' ||
+			s[a - 1] == ')' ||
+			s[a - 1] == '{' ||
+			s[a - 1] == '}')
+	{
+		s[a] = s[a] - 32;
+	}
+	a++;
+	}
+	return (s);
+}
