732168d5adc529ab15af8f90354d5925a7298c51
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-02 12:31:26 -0800

    3.3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,23 +7,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int a, b, c = 0;
+	int a, b, c = 0, d;
 
 	a = 0;
 	while (s[a] != '\0')
 	{
+		d = 0;
 		b = 0;
 		while (accept[b] != '\0')
 		{
 			if (accept[b] == s[a])
 			{
 				c++;
-			}
-			else
-			{
-				return (c);
+				d = 1;
 			}
 			b++;
+		}
+		if (d == 1)
+		{
+			break;
 		}
 		a++;
 	}
