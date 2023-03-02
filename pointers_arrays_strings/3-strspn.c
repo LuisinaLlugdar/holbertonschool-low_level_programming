9434f6c3291b9af7edcd80aaa42cb85577d2c84a
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-02 12:04:05 -0800

    3.3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a substring
+ * @s: substring
+ * @accept: string
+ * Return: number of coincidences
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b, c = 0;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (accept[b] != '\0')
+		{
+			if (accept[b] == s[a])
+			{
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (c);
+}
