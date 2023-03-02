4da6cf5ea79aa2bb9f974293a5115077b6e329d6
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-02 08:09:39 -0800

    1.3

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: int
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	unsigned int j;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j])
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
+}
