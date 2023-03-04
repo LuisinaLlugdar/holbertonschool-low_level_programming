bf06521791d23ff10cbde94f34aa7200a3f74376
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-03 17:16:26 -0800

    8.3

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,7 @@ void print_diagsums(int *a, int size)
 
 	d1 = 0;
 	d2 = 0;
-	for(b = 0; b < size; b++)
+	for (b = 0; b < size; b++)
 	{
 		d1 += *(a + b * size + b);
 		d2 += *(a + b * size + (size - b - 1));
