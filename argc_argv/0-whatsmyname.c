afd68faf07dec15cc4963599106065c568257b06
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-09 08:55:34 -0800

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - code
+ *
+ * @argc: variable
+ *
+ * @argv: variable
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
