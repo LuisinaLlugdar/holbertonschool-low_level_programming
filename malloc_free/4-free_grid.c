91ccbc22e38d31d9f1ff08ad1c118070a3947811
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-14 12:25:38 -0700

    4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a grid
+ * @grid: grid made with alloc_grid function
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a <= height; a++)
+		free(grid[a]);
+	free(grid);
+
+	if (grid == NULL)
+		free(grid);
+}
---
+++ malloc_free/_strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ *
+ * Return: resulting string dest
+ *
+ * @dest: variable
+ *
+ * @src: variable
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+Footer
+© 2023 GitHub, Inc.
