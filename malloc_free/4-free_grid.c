e05ec079e58a606a0e886177b18348ce9ac6c035
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-14 12:37:08 -0700

    4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,10 +10,10 @@ void free_grid(int **grid, int height)
 {
 	int a;
 
-	for (a = 0; a <= height; a++)
-		free(grid[a]);
-	free(grid);
-
 	if (grid == NULL)
 		free(grid);
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid);
 }
