5b0369eca33f1e81729ddbe2a4f925312c2d3301
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-24 07:06:48 -0700

    varfunc3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints everything
+ * @format: list of types of arguments passed to the function.
+ */
+void print_all(const char * const format, ...)
+{
+	char *separator = "";
+	int a, b;
+	va_list list;
+
+	type_d option[] = {
+		{'c', printc},
+		{'i', printi},
+		{'f', printfl},
+		{'s', prints},
+		{'\0', NULL}
+	};
+
+	va_start(list, format);
+	b = 0;
+	while (format[b] != '\0' && format != NULL)
+	{
+		a = 0;
+		while (option[a].dtype != '\0')
+		{
+			if (option[a].dtype == format[b])
+			{
+				printf("%s", separator);
+				option[a].f(list);
+				separator = ", ";
+			}
+			a++;
+		}
+		b++;
+	}
+	va_end(list);
+	printf("\n");
+}
+
+/**
+* printc - function that prints char
+* @list: va_list
+*/
+void printc(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+* printi - function that prints int
+* @list: va_list
+*/
+void printi(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+* printfl - function that prints float
+* @list: va_list
+*/
+void printfl(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+* prints - function that prints strings
+* @list: va_list
+*/
+void prints(va_list list)
+{
+	char *string;
+
+	string = va_arg(list, char *);
+
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -10,5 +10,19 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void printc(va_list list);
+void printi(va_list list);
+void printfl(va_list list);
+void prints(va_list list);
 
+/**
+ * struct type - struct
+ * @dtype: char meaning the data type
+ * @f: function
+ */
+typedef struct type
+{
+	char dtype;
+	void (*f)(va_list);
+} type_d;
 #endif
