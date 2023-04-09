4255b17ac3f159a156f3bb2d6ffebbe287fdb6a4
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-04-09 14:24:25 -0700

    task0 add comments

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,23 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
+	/*obtengo el largo de b, que le hare el left shift para obtener la...*/
+	/*cantidad de 0 necesaria para tener la potencia de 2 que corresponde*/
 	while (b[length] != '\0')
 		length++;
+	/*para 2^0 no necesito correr ningun lugar, corro en todos los casos*/
+	/*menos en uno*/
 	length = length - 1;
 
+	/*recorro b en busca de '1', que se multiplicara por alguna pot de 2*/
 	while (b[x] != '\0')
 	{
 		if ((b[x] != '0') && (b[x] != '1'))
 			return (0);
 
+		/*cuando encuentra un 1, lo multiplica por el 1 en binario...*/
 		if (b[x] == '1')
+		/*movido len lugares, que sucesiv. me va dando las pot de 2*/
 			sum = sum + (1 * (1 << length));
 		x++;
 		length--;
