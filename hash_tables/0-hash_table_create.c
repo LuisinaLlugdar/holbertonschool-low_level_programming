4bd9e91e355ce92227a3cbf846dc144e06b41497
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-04-13 06:30:47 -0700

    task 0, comments

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,26 +6,33 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	/*pointer to struct that creates a new hash table*/
 	hash_table_t *new_ht;
 	unsigned long int i;
 
+	/*if size doesn't exist, the hash table either*/
 	if (size == 0)
 		return (NULL);
 
+	/*memory to allocate everything that the struct hash_table_t has*/
 	new_ht = malloc(sizeof(hash_table_t));
 	if (new_ht == NULL)
 		return (NULL);
 
 	new_ht->size = size;
+	/*memory to allocate "size" number of nodes*/
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 
+	/*if malloc for the array fails...*/
 	if (new_ht->array == NULL)
 	{
+		/*...free the previously allocated memory*/
 		free(new_ht);
 		return (NULL);
 	}
 
 	i = 0;
+	/*inicialize every index of the array to NULL*/
 	while (i < size)
 	{
 		new_ht->array[i] = NULL;
